b864c9f3eeec32911d416330f778419e8111d942
ashwin bicholiya <47949413+Ashwinbicholiya@users.noreply.github.com>
2020-10-19 23:11:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+resizeToAvoidBottomInset: false,
       drawer: Drawer(
         child: Column(
           children: <Widget>[
